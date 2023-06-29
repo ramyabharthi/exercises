d988dbe20bc9bfd41abd16c2afc105a81434b00d
Ramya <ramyacandy5@gmail.com>
2023-06-29 16:37:19 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script >
+import FormList from './components/FormList.vue'
+export default {
+  name: 'App',
+  components: {
+          FormList
+  }
+}
+</script>
+
+<template>
+  <FormList/>
+</template>
+
+<style >
+
+</style>
